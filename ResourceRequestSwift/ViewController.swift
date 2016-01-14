a4085ae1596b1b2c0f61eb66cf6e076a3c9480ca
Nathan Hazout <nathanh@il.ibm.com>
2016-01-14 13:59:21 +0200

    First version

--- ResourceRequestSwift/ViewController.swift
+++ ResourceRequestSwift/ViewController.swift
@@ -7,19 +7,62 @@
 //
 
 import UIKit
+import IBMMobileFirstPlatformFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var openButton: UIButton!
+    
+    var pickerData: [String] = [String]()
+    var selectedTag = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        pickerData = ["MobileFirst_Platform", "MobileFirst_Playground", "Item 3", "Item 4", "Item 5", "Item 6"]
+        self.picker.dataSource = self;
+        self.picker.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func openPicker() {
+        self.picker.hidden = false
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
 
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.selectedTag = pickerData[row]
+        self.openButton.setTitle(self.selectedTag, forState: .Normal)
 
+        self.picker.hidden = true
+    }
+    @IBAction func sendRequest(sender: UIButton) {
+        let request = WLResourceRequest(URL: NSURL(string: "/adapters/JavaHTTP/"), method: WLHttpMethodGet)
+        request.setQueryParameterValue(self.selectedTag, forName: "tag")
+        request.sendWithCompletionHandler { (response, error) -> Void in
+            if(error == nil){
+                NSLog(response.description)
+            }
+            else{
+                NSLog(error.description)
+            }
+        }
+        
+    }
 }
 
